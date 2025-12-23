cd6efd131f584c5c468bef806b11d22e5eb1c5e1
zoitovfirdavs3-eng <zoitovfirdavs3@gmail.com>
2025-12-23 12:29:04 +0500

    Initial commit

---
+++ fs/writeFileSync.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+fs.writeFileSync("greeting.txt", "Hello, World!");
---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require("cowsay");
+
+console.log(fs.say({
+    text : "Hello, World!",
+    e : "oo"
+}));
